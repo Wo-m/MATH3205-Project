27efe4225b681de63298fd8c5d0fd6905486a0cd
Sam Smeaton <samsmeaton24@gmail.com>
2022-09-22 17:20:27 +1000

    fixed up recursive fn

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import data_read
 depot_data, jobs_data, travel_data, general_data, vehicle_data = data_read.get_data(1, 10, 3, 1)
 
 # reus is people, non reus is equipment
+DEPOT_JOB = 0
 
 # periods = int(general_data[0])
 # jobs = int(general_data[1])  # also num of turbines
@@ -12,12 +13,13 @@ depot_data, jobs_data, travel_data, general_data, vehicle_data = data_read.get_d
 # people_types = int(general_data[3])
 
 periods = 1
-jobs = 2
+jobs = 3
 vehicles = 1
 people_types = 1
 
 # Create the nodes size = 2n+2 with final two nodes as depot pickup and delivery, n = num jobs
 N = 2*jobs + 2
+R = 0
 
 routes = {}  # route for vehicle v in period t on route r (dimensions will be expanded)
 cost = {}  # cost for vehicle v in period t on route r (dimensions will be expanded)
@@ -31,24 +33,21 @@ def get_routes():
 def generate_routes():
 	for t in range(periods):
 		for v in range(vehicles):
-			r_index = 0  # index of the route being created
-			j_start = 0
-			recursive_generation(v, t, [N + 1], r_index, j_start)
+			recursive_generation(v, t, [DEPOT_JOB], 1)
 
 
-def recursive_generation(v, t, J, r_index, j_start):
-	r = r_index
-	job_set = J.copy()
-	for j in range(j_start, jobs):
-		job_set.append(j)
-		r = solve_route(v, t, job_set, r)  # increment route index if feasible route
-		recursive_generation(v, t, job_set, r, j_start + 1)  # increment both r and j indexes
+def recursive_generation(v, t, J, j_start):
+	for j in range(j_start, jobs + 1):
+		Jdash = J.copy()
+		Jdash.append(j)
+		solve_route(v, t, Jdash)
+		recursive_generation(v, t, Jdash, j + 1)
 
 
-def solve_route(v, t, J, r_index):
-	routes[v, t, r_index] = J
-	r = r_index + 1  # update index if feasible
-	return r
+def solve_route(v, t, J):
+	global R
+	routes[v, t, R] = J
+	R += 1  # update index if feasible
 
 
 
